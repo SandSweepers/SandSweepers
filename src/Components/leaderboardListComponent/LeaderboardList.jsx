769bdd3a91e766550160938f87ea7a990ccb14d1
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-04-03 23:47:58 +0100

    enhance leaderboard and beach card components with improved styling and data structure

--- src/Components/leaderboardListComponent/LeaderboardList.jsx
+++ src/Components/leaderboardListComponent/LeaderboardList.jsx
@@ -10,33 +10,40 @@ export const LeaderboardList = () => {
             .then(response => response.json())
             .then(data => {
                 const sortedUsers = data.users.sort((a, b) => b.points - a.points); 
-                setDummyData(sortedUsers); // all users
+                setDummyData(sortedUsers);
             })
             .catch(error => {
-                console.error('Error fetching dummy data:', error);
+                console.error('Error fetching dum fucking data:', error);
             });
     }, []);
 
     const handleShowMore = () => {
-        setDisplayCount((prevCount) => prevCount + 3); // Alice change the number to what works for design
+        setDisplayCount((prevCount) => prevCount + 3); 
     };
 
     return (
-        <>
-            <h1>Leaderboard</h1>
+        <div className="leaderboard-list">
+            <div className="list-header">
+                <div>Friends</div>
+                <div>Hours</div>
+                <div>Points</div>
+            </div>
+            
             <ul>
-                {dummyData.slice(3, 3 + displayCount).map((user) => (
+                {dummyData.slice(3, 3 + displayCount).map((user, index) => (
                     <li key={user.id}>
+                        <div className="rank">{index + 4}</div>
                         <img src={user.profile_picture} alt={user.username} />
-                        <h3>Name: {user.username}</h3>
-                        <p>PTS: {user.points}</p>
-                        <p>Time: {user.total_time_spent} Hours</p>
+                        <div className="name">{user.username}</div>
+                        <div className="hours">{user.total_time_spent}</div>
+                        <div className="points">{user.points} pts</div>
                     </li>
                 ))}
             </ul>
+            
             {displayCount + 3 < dummyData.length && ( 
                 <button onClick={handleShowMore}>Show More</button>
             )}
-        </>
+        </div>
     );
 };
--- src/PageComponents/HomePageComponent/HomePage.jsx
+++ src/PageComponents/HomePageComponent/HomePage.jsx
@@ -1,3 +1,4 @@
+import { BeachCard } from '../../Components/BeachCardsComponent/BeachCards';
 import { Leaderboard } from '../../Components/LeaderboardComponent/Leaderboard';
 import './HomePage.scss'
 
@@ -6,6 +7,7 @@ export const HomePage = () => {
   return (
     <><h1>home</h1>  
     <Leaderboard/>
+    <BeachCard/>
 </>
 );
 };
