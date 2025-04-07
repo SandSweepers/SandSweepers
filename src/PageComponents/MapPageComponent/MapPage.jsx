f2936e6664361561d0ec5a6b2a0f38ac9d0281d6
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-04-07 10:54:40 +0100

    add a blank line for improved readability in LeaderboardList component and added map prototype

--- src/PageComponents/MapPageComponent/MapPage.jsx
+++ src/PageComponents/MapPageComponent/MapPage.jsx
@@ -1,7 +1,34 @@
-import './MapPage.scss'
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './MapPage.scss';
 
 export const MapPage = () => {
+    useEffect(() => {
+        const map = L.map('map-container', {
+            center: [28.0033198, -15.4163828], // Updated center coordinates
+            zoom: 13, // Updated zoom level
+            scrollWheelZoom: false, // Disable scroll wheel zoom
+        });
 
-  return (
-<h1>Map</h1>  );
+        // Add a tile layer (e.g., OpenStreetMap)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        // Add a marker as an example
+        L.marker([28.0033198, -15.4163828]).addTo(map).bindPopup('Gran Canaria').openPopup();
+
+        return () => {
+            map.remove(); // Clean up the map instance on component unmount
+        };
+    }, []);
+
+    return <div id="map-container" className="map-container"></div>;
 };
+
+
+
+
+
+
